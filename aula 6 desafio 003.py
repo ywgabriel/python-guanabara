a5c722c54c5fe0dc370cc8f37d32a84bb684717f
Gabriel Gonçalves <111677837+ywgabriel@users.noreply.github.com>
2022-08-21 23:00:44 -0300

    Add files via upload

---
+++ aula 6 desafio 003.py
@@ -0,0 +1,7 @@
+print('Boas-vindas, usuário! Esta é versão 1.02 da Somadora, a calculadora mais limitada existente :)')
+print('Vamos começar?')
+n1 = int(input('Digite o primeiro valor: '))
+n2 = int(input('Digite o segundo valor: '))
+s = n1 + n2
+print('A soma entre {} e {} é: {}' .format(n1, n2, s))
+print(':)')
