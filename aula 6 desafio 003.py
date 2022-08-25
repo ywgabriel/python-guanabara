e23c56aeb97b734d24df37a59dde7c0730a0c008
Laura Pessine <90737056+laurapessine@users.noreply.github.com>
2022-08-25 10:08:59 -0300

    remove space between string and format
    In general, there are never any spaces between a variable and its method (in this case, between the string 'A soma entre {} e {} é: {}' and its method .format()) 😉

--- aula 6 desafio 003.py
+++ aula 6 desafio 003.py
@@ -3,5 +3,5 @@ print('Vamos começar?')
 n1 = int(input('Digite o primeiro valor: '))
 n2 = int(input('Digite o segundo valor: '))
 s = n1 + n2
-print('A soma entre {} e {} é: {}' .format(n1, n2, s))
+print('A soma entre {} e {} é: {}'.format(n1, n2, s))
 print(':)')
