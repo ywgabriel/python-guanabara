7744b9292f27b8331277b883f912784205105725
Gabriel Gonçalves <111677837+ywgabriel@users.noreply.github.com>
2022-09-07 23:55:52 -0300

    aplicador de 5% de desconto

--- aula 007 desafio 012.py
+++ aula 007 desafio 012.py
@@ -1,7 +1,7 @@
 # - Desafio 012
 # Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 
-preço = float(input('Qual o preço do seu produto? R$  '))
-desconto = preço * 0.05
-desconto2 = preço * 0.95
+preco = float(input('Qual o preço do seu produto? R$  '))
+desconto = preco * 0.05
+desconto2 = preco * 0.95
 print(f'Seu produto com um desconto de 5% vai custar: R$ {desconto2:.2f}.')
