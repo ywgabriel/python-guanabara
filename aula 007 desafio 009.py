7e5fa6b8c5396bb82bf5764d5503f31c68d23cba
Gabriel Gonçalves <111677837+ywgabriel@users.noreply.github.com>
2022-09-07 22:05:27 -0300

    tabuada de um número inteiro

---
+++ aula 007 desafio 009.py
@@ -0,0 +1,15 @@
+# - Desafio 009
+# Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
+
+n = int(input('Digite um número inteiro: '))
+print('A tabuada (até o 10) desse número é: ')
+print(f'1 X {n} = {n*1}')
+print(f'2 X {n} = {n*2}')
+print(f'3 X {n} = {n*3}')
+print(f'4 X {n} = {n*4}')
+print(f'5 X {n} = {n*5}')
+print(f'6 X {n} = {n*6}')
+print(f'7 X {n} = {n*7}')
+print(f'8 X {n} = {n*8}')
+print(f'9 X {n} = {n*9}')
+print(f'10 X {n} = {n*10}')
