aa66a52c76285963183462b002e1405e23f44df4
Gabriel Gonçalves <111677837+ywgabriel@users.noreply.github.com>
2022-09-07 23:36:35 -0300

    * update * comprador de dólares
    (preço desatualizado)

--- aula 007 desafio 010.py
+++ aula 007 desafio 010.py
@@ -1,8 +1,8 @@
 # - Desafio 010
-# Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos Dólares ela pode comprar.
-# Considere US$1,00 = R$3,27
+# Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
+# Considere US$ 1,00 = R$ 3,27
 
-r = float(input('Quantos reais você tem na sua carteira? '))
-d = 3.27
-c = r//d
-print(f'Com a quantia de R${r}0 , você poderia comprar US${c}0 .')
+reais = float(input('Quantos reais você tem na sua carteira? R$ '))
+valor_dolar = 3.27
+dolares = reais // valor_dolar
+print(f'Com a quantia de R$ {reais:.2f}, você poderia comprar US$ {dolares:.2f}.')
