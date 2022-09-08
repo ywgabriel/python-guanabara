1fe0c8188302572e77be68f0ca8439792608fce8
Gabriel Gonçalves <111677837+ywgabriel@users.noreply.github.com>
2022-09-07 22:02:14 -0300

    média de duas notas de um aluno

---
+++ aula 007 desafio 007.py
@@ -0,0 +1,7 @@
+# - Desafio 007
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre sua média.
+
+nota1 = int(input('Qual foi sua nota da primeira prova de matemática? '))
+nota2 = int(input('E qual foi a sua segunda nota da segunda prova? '))
+m = (nota1+nota2)/2
+print(f'Então sua média de matemática é: {m}')
