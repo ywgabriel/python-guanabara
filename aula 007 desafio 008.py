17c0419d68c6a9184a6c0654e05ee105b0f80273
Gabriel Gonçalves <111677837+ywgabriel@users.noreply.github.com>
2022-09-07 22:03:33 -0300

    conversor de metros para centímetros e milímetros

---
+++ aula 007 desafio 008.py
@@ -0,0 +1,8 @@
+# - Desafio 008
+# Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+
+m = int(input('Digite aqui o valor em metros(m): '))
+cm = m*100
+mm = m%1000
+print(f'O valor em centímetros(cm) será de: {cm} cm')
+print(f'O valor em milímetros(mm) será de: {mm} mm')
