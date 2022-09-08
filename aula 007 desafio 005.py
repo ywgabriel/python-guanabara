cbd989f4c8dd323379701ca654853efb7a204b21
Gabriel Gonçalves <111677837+ywgabriel@users.noreply.github.com>
2022-09-07 22:00:06 -0300

    sucessor e antecessor de um número inteiro

---
+++ aula 007 desafio 005.py
@@ -0,0 +1,6 @@
+# - Desafio 005
+# Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+n1 = int(input('Digite um número: '))
+print(f'O antecessor desse número é: {n1-1}')
+print(f'E o sucessor desse número é: {n1+1}')
