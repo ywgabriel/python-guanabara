ccbfaaa5582a07ab846f4f80eaaac8bbc584c446
Gabriel Gonçalves <111677837+ywgabriel@users.noreply.github.com>
2022-09-07 23:45:55 -0300

    dobro, triplo e raiz quadrada de um número
    (mini update - separando operadores das variáveis)

--- aula 007 desafio 006.py
+++ aula 007 desafio 006.py
@@ -2,6 +2,6 @@
 # Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
 
 n1 = int(input('Digite um número: '))
-print(f'O dobro desse número é: {n1*2}')
-print(f'Seu triplo é: {n1*3}')
-print(f'E sua raiz quadrada é: {n1**(1/2)}')
+print(f'O dobro desse número é: {n1 * 2}')
+print(f'Seu triplo é: {n1 * 3}')
+print(f'E sua raiz quadrada é: {n1 ** (1/2)}')
