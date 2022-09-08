b9496d4213a0589efb965d9a9ab865d74e7401da
Gabriel Gonçalves <111677837+ywgabriel@users.noreply.github.com>
2022-09-07 23:42:59 -0300

    média de duas notas de um aluno
    (mini update! - troquei o ''m'' por ''media'' pra ficar mais entendível, e separei os operadores das variáveis 👍)

--- aula 007 desafio 007.py
+++ aula 007 desafio 007.py
@@ -3,5 +3,5 @@
 
 nota1 = int(input('Qual foi sua nota da primeira prova de matemática? '))
 nota2 = int(input('E qual foi a sua segunda nota da segunda prova? '))
-m = (nota1+nota2)/2
-print(f'Então sua média de matemática é: {m}')
+media = (nota1 + nota2) / 2
+print(f'Então sua média de matemática é: {media}')
