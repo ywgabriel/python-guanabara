99fc46742b19f1c47154e690433792e7a72cf828
Gabriel Gonçalves <111677837+ywgabriel@users.noreply.github.com>
2022-09-07 23:57:32 -0300

    aplicando 15% de aumento em um salário

--- aula 007 desafio 013.py
+++ aula 007 desafio 013.py
@@ -1,6 +1,6 @@
 # - Desafio 013
 # Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
 
-s1 = float(input('De quanto foi seu salário do mês passado, antes do aumento de 15% desse mês? '))
-a = s1*1.15
-print(f'Então seu salário desse mês vai ser de: R${a}O .')
+salario1 = float(input('De quanto foi seu salário do mês passado, antes do aumento de 15% desse mês? '))
+salario2 = salario1 * 1.15
+print(f'Então seu salário desse mês vai ser de: R$ {salario2:.2f}.')
