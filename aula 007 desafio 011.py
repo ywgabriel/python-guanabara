679e87fa5ac24dfef7281a921fde5434051be73b
Gabriel Gonçalves <111677837+ywgabriel@users.noreply.github.com>
2022-09-07 22:09:23 -0300

    medidor de tinta para uma parede

---
+++ aula 007 desafio 011.py
@@ -0,0 +1,10 @@
+# - Desafio 011
+# Faça um programa que leia a largura e a altura de uma parede em metros.
+# Calcule sua área e a quantidade de tinta necessária para pintá-la. Considere: 1 litro de tinta = área de 2m² pintada.
+
+largura = float(input('Qual a largura (em metros) da parede que vai ser pintada? '))
+altura = float(input('E qual é a altura (em metros novamente) da parede? '))
+area = largura*altura
+l = area/2
+print(f'Sua parede possui uma área de {area}m²')
+print(f'Sabendo que cada litro de tinta pinta uma área de 2m², vai ser necessário {l} litros de tinta para pintar a parede inteira')
