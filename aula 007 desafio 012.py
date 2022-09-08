3845907aa1f08e19f457b250d0ef2ee339ca7e84
Gabriel Gonçalves <111677837+ywgabriel@users.noreply.github.com>
2022-09-07 23:53:21 -0300

    aplicador de 5% de desconto
    (update - separei variáveis e operadores, formatei as casas decimais tbm com ''.2f'', mudei também o nome de algumas coisas, como "p" que foi pra "preço" e os descontos, que eram "d" e "d2", foram pra "desconto" e "desconto2" - só pra deixar mais bonitinho no código msm - 👍 ).

--- aula 007 desafio 012.py
+++ aula 007 desafio 012.py
@@ -1,7 +1,7 @@
 # - Desafio 012
 # Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 
-p = float(input('Qual o preço do seu produto (em R$)? '))
-d = p*0.05
-d2 = p*0.95
-print(f'Seu produto com um desconto de 5% vai custar: R${d2}0 .')
+preço = float(input('Qual o preço do seu produto? R$  '))
+desconto = preço * 0.05
+desconto2 = preço * 0.95
+print(f'Seu produto com um desconto de 5% vai custar: R$ {desconto2:.2f}.')
