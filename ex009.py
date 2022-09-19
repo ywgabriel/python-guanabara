a361896cbf5ef12d27312161aff342cc803577e2
Gabriel Gonçalves <111677837+ywgabriel@users.noreply.github.com>
2022-09-18 23:28:12 -0300

    Tabuada

---
+++ ex009.py
@@ -0,0 +1,14 @@
+# - Exercício 009
+# Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
+
+numero = int(input('Digite um número para ver sua tabuada: '))
+print(f'{numero} X 1 = {numero * 1}')
+print(f'{numero} X 2 = {numero * 2}')
+print(f'{numero} X 3 = {numero * 3}')
+print(f'{numero} X 4 = {numero * 4}')
+print(f'{numero} X 5 = {numero * 5}')
+print(f'{numero} X 6 = {numero * 6}')
+print(f'{numero} X 7 = {numero * 7}')
+print(f'{numero} X 8 = {numero * 8}')
+print(f'{numero} X 9 = {numero * 9}')
+print(f'{numero} X 10 = {numero * 10}')
